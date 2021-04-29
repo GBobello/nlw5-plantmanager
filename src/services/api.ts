663f5411425e6b859c4ab4eee39548d07ad3fb59
GBobello <bobello.gabriel@gmail.com>
2021-04-29 14:06:34 -0300

    Adicionando licensa e server

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://192.168.100.22:3333',
+    baseURL: 'https://my-json-server.typicode.com/GBobello/nlw5-plantmanager',
 })
 
 export default api;
